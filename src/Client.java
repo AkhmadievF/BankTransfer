a237657ae7d115272dab9a91d79f6258eab7c633
AkhmadievF <fa.90@bk.ru>
2025-05-08 20:37:35 +0500

    AddClientClass

--- src/Client.java
+++ src/Client.java
@@ -3,4 +3,10 @@ public class Client {
     int ID;
     String BankAccountNumber1;
     String BankAccountNumber2;
+    String BankAccountNumber3;
+
+    public Client(String name, int ID) {
+        this.name = name;
+        this.ID = ID;
+    }
 }
