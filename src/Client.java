ec6020261b1a7f56d64a2827e06ec2d286c7e5d3
AkhmadievF <fa.90@bk.ru>
2025-05-08 20:45:14 +0500

    AddClientClass

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,6 @@
 public class Client {
     String name;
     int ID;
-    String BankAccountNumber1;
-    String BankAccountNumber2;
-    String BankAccountNumber3;
 
     public Client(String name, int ID) {
         this.name = name;
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         Client client1 = new Client("Petrov", 1);
+        System.out.println(client1.ID);
     }
 }
