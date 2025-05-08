b1360e2fa52038dfa9fcc5be08fc48f650a4368e
AkhmadievF <fa.90@bk.ru>
2025-05-08 20:47:29 +0500

    AddClientClass

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Client client1 = new Client("Petrov", 1);
         System.out.println(client1.ID);
+        System.out.println(client1.ID);
+        System.out.println(client1.ID);
     }
 }
